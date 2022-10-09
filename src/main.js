24b4ad703a4f8bfab2f470109e9e917e45535bb1
gazmirmazari <gazmir.mazari@foureyes.io>
2022-10-09 11:53:33 -0400

    set up the element ui dashboard

--- src/main.js
+++ src/main.js
@@ -1,16 +1,17 @@
-import { createApp } from 'vue';
-import { createPinia } from 'pinia';
-
-import App from './App.vue';
-import { router } from './router';
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import App from "./App.vue";
+import { router } from "./router";
 
 // setup fake backend
-import { fakeBackend } from './helpers';
+import { fakeBackend } from "./helpers";
 fakeBackend();
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-
-app.mount('#app');
+app.use(ElementPlus);
+app.mount("#app");
